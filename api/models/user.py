4bf3306708c397ea9be8a19bec7b74080bb0fb23
garrettpyke <garrettpyke@msn.com>
2024-09-02 17:30:11 -0600

    Now have a working custom User model

--- api/models/user.py
+++ api/models/user.py
@@ -1,72 +1,55 @@
-from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
 from django.db import models
+from django.contrib.auth.models import BaseUserManager, AbstractBaseUser
+
 
 class MyUserManager(BaseUserManager):
-    def create_user(self, email, password=None, **kargs):
+    def create_user(self, email, password=None):
         """
         Creates and saves a User with the given email and password.
         """
-        # Add a custom validation error
         if not email:
-            raise ValueError('User must have an email address!') 
-        # Create a user from the UserModel
-        # Use the normalize_email method from the BaseUserManager to
-        # normalize the domain of the email
-        # We'll also unwind the extra fields.  Remember that two asterisk (**)
-        # in Python refers to the extra keyword arguments that are passed into
-        # a function (meaning these are key=value pairs).
-        user = self.model(email=self.normalize_email(email), **kargs)
+            raise ValueError("Users must have an email address")
 
-        # Use the set_password method to hash the password
+        user = self.model(
+            email=self.normalize_email(email)
+        )
+
         user.set_password(password)
-
-        # Call save to save the user to the database
-        user.save()
-
-        # Always return the user!
+        user.save(using=self._db)
         return user
-    
-    def create_superuser(self, email, password, **kargs):
+
+    def create_superuser(self, email, password=None):
         """
         Creates and saves a superuser with the given email and password.
         """
-        # Use the custom create_user method above to create
-        # the user.
-        user = self.create_user(email, password, **kargs)
-        # Add the required is_superuser and is_staff properties
-        # which must be set to True for superusers
-        user.is_superuser = True
-        user.is_staff = True
-        # Save the user to the database with the new properties
-        user.save()
-
-        # Always return the user!
+        user = self.create_user(
+            email,
+            password=password
+        )
+        user.is_admin = True
+        user.save(using=self._db)
         return user
-    
-class MyUser(AbstractBaseUser, PermissionsMixin):
-    # Add custom fields here
-    # first_name = models.CharField(max_length=30, blank=True)
-    # last_name = models.CharField(max_length=30, blank=True)
-    email = models.EmailField(unique=True)
+
+
+class MyUser(AbstractBaseUser):
+    email = models.EmailField(
+        verbose_name="email address",
+        max_length=255,
+        unique=True
+    )
+    # date_of_birth = models.DateField()
     is_active = models.BooleanField(default=True)
-    is_staff = models.BooleanField(default=False)
-    
+    is_admin = models.BooleanField(default=False)
+
+    # `objects = MyUserManager()` is defining a custom manager for the `MyUser` model.
     objects = MyUserManager()
-    
-    USERNAME_FIELD = 'email'
-    # REQUIRED_FIELDS = []
-    
-        # Create a user from the UserModel
-        # Use the normalize_email method from the BaseUserManager to
-        # normalize the domain of the email
-        # We'll also unwind the extra fields.  Remember that two asterisk (**)
-        # in Python refers to the extra keyword arguments that are passed into
-        # a function (meaning these are key=value pairs).
-        # user = self.model(email=self.normalize_email(email), **extra_fields)
-    
+
+    USERNAME_FIELD = "email"
+    # REQUIRED_FIELDS = ["date_of_birth"]
+
     def __str__(self):
         return self.email
-    
+
     def has_perm(self, perm, obj=None):
         "Does the user have a specific permission?"
         # Simplest possible answer: Yes, always
@@ -82,4 +65,3 @@ class MyUser(AbstractBaseUser, PermissionsMixin):
         "Is the user a member of staff?"
         # Simplest possible answer: All admins are staff
         return self.is_admin
-    
